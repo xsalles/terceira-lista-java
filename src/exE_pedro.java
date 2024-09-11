eaf7530def289bcd0d500d1d734ef4d669aefbc9
xsalles <xs.salles@gmail.com>
2024-09-10 21:24:07 -0300

    feat: exercise E.

---
+++ src/exE_pedro.java
@@ -0,0 +1,9 @@
+public class exE_pedro {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++){
+            if (i % 2 != 0){
+                System.out.println("Os números impares são " + i);
+            }
+        }
+    }
+}
