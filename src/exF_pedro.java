7cd5b8a72857e449e519724e033709b99e789899
xsalles <xs.salles@gmail.com>
2024-09-10 21:27:32 -0300

    feat: exercise F.

---
+++ src/exF_pedro.java
@@ -0,0 +1,9 @@
+public class exF_pedro {
+    public static void main(String[] args) {
+        for (int i = 0; i < 200; i++){
+            if (i % 4 == 0 && i < 200){
+                System.out.println(i);
+            }
+        }
+    }
+}
