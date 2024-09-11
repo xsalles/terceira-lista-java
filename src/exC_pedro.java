6647f7d21461bd80c0ff5a5c58e26e27290c9d8d
xsalles <xs.salles@gmail.com>
2024-09-10 21:01:16 -0300

    feat: exercise C.

---
+++ src/exC_pedro.java
@@ -0,0 +1,7 @@
+public class exC_pedro {
+    public static void main(String[] args) {
+        int soma = 50 * 101; //Soma de Gauss
+
+        System.out.println("A soma dos números de 1 até 100 é de " + soma);
+    }
+}
