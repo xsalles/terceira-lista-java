085a5fdb313fa1115d68f1178aa3485a71e0a237
xsalles <xs.salles@gmail.com>
2024-09-30 14:22:03 -0300

    refactored: removed the if on loop for.

--- src/exE_pedro.java
+++ src/exE_pedro.java
@@ -1,9 +1,7 @@
 public class exE_pedro {
     public static void main(String[] args) {
-        for (int i = 0; i <= 20; i++){
-            if (i % 2 != 0){
-                System.out.println("Os números impares são " + i);
-            }
+        for (int i = 1; i <= 20; i += 2){
+                System.out.println("NÚMEROS ÍMPARES: " + i);      
         }
     }
 }
