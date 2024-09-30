0f6a81ff38f487a0aaf2b0a6271bfaa9419d3f01
xsalles <xs.salles@gmail.com>
2024-09-30 14:30:08 -0300

    refactored the code and understand what exercise ordered to me.

--- src/exA_pedro.java
+++ src/exA_pedro.java
@@ -1,18 +1,10 @@
 import java.util.Arrays;
 
-public class exA_pedro{
+public class exA_pedro {
     public static void main(String[] args) {
-        int valorDasRaizes = 0;
 
-        for (int i = 15; i <= 200; i++){
-            valorDasRaizes = i*i;
-            System.out.println(valorDasRaizes);
+        for (int i = 15; i <= 200; i++) {
+            System.out.printf("\n[%d]\n", i);
         }
-
-        int[] raizesQuadradas = new int[]{valorDasRaizes};
-
-        Arrays.sort(raizesQuadradas);
-
-        System.out.println(Arrays.toString(raizesQuadradas));
     }
 }
