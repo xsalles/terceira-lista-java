656aa05aba9f7eafad27905a5117439bfea1d181
xsalles <xs.salles@gmail.com>
2024-09-30 14:26:29 -0300

    removed the sout with all number until ten, and created a loop with for.

--- src/exB_pedro.java
+++ src/exB_pedro.java
@@ -3,21 +3,16 @@ import java.util.Scanner;
 public class exB_pedro {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System .in);
-        int valor;
+        int value;
 
-        System.out.println("Digite o valor que você deseja saber a tabuada");
-        valor = sc.nextInt();
+        System.out.println("\nDigite o valor que você deseja saber a tabuada\n");
+        value = sc.nextInt();
+        
+        System.out.printf("\nTABUADA DO %d\n\n", value);
 
-        System.out.println(valor * 1 
-                         + "\n" + valor * 2 
-                         + "\n" + valor * 3 
-                         + "\n" + valor * 4 
-                         + "\n" + valor * 5 
-                        + "\n" + valor * 6 
-                        + "\n" + valor * 7 
-                        + "\n" + valor * 8 
-                        + "\n" + valor * 9 
-                        + "\n" + valor * 10 + "\n");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(value * i);
+        }
 
         sc.close();
     }
